f8aff6d7216547ea8b5806fdd734b7d464b64e83
Ahmad Fahmi Aisar <fahmiaisar99@gmail.com>
2021-02-11 04:20:25 +0700

    setup

---
+++ submission1/app/src/main/java/com/example/submission1/data/vo/HttpResult.kt
@@ -0,0 +1,10 @@
+package com.example.submission1.data.vo
+
+enum class HttpResult {
+    NO_CONNECTION,
+    TIMEOUT,
+    CLIENT_ERROR,
+    BAD_RESPONSE,
+    SERVER_ERROR,
+    NOT_DEFINED,
+}
---
+++ submission1/app/src/main/java/com/example/submission1/util/Extension.kt
@@ -0,0 +1,46 @@
+package com.example.submission1.util
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.view.View
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.RequestOptions
+
+fun ImageView.loadUrl(url: String) {
+    Glide.with(this.context)
+        .load(url)
+        .thumbnail(0.3f)
+        .placeholder(ColorDrawable(Color.LTGRAY))
+        .transition(DrawableTransitionOptions.withCrossFade())
+        .apply(RequestOptions.fitCenterTransform())
+        .into(this)
+}
+
+@BindingAdapter("loadImage")
+fun loadPotraitImage(view: ImageView, imageUrl: String?) {
+    if (imageUrl.isNullOrEmpty()) return
+
+    val url = "https://image.tmdb.org/t/p/w185/$imageUrl"
+
+    Glide.with(view.context)
+        .load(url)
+        .transition(DrawableTransitionOptions.withCrossFade())
+        .thumbnail(0.2f)
+        .placeholder(ColorDrawable(Color.LTGRAY))
+        .into(view)
+}
+
+fun View.visible() {
+    this.visibility = View.VISIBLE
+}
+
+fun View.invisible() {
+    this.visibility = View.INVISIBLE
+}
+
+fun View.gone() {
+    this.visibility = View.GONE
+}
